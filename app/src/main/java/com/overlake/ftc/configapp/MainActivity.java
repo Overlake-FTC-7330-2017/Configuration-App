1e205092283eea61a5893d24aa5c049bb075530c
unholydeath785 <ivansonofcoul@gmail.com>
2017-09-15 19:53:47 -0700

    added current progress

---
+++ app/src/main/java/com/overlake/ftc/configapp/MainActivity.java
@@ -0,0 +1,56 @@
+package com.overlake.ftc.configapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.ConsoleMessage;
+
+import java.io.Console;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        switch(id) {
+            case R.id.action_create_config:
+                startActivity(new Intent(MainActivity.this, CreateConfigActivity.class));
+                return true;
+            case R.id.action_view_config:
+                return true;
+            case R.id.action_edit_config:
+                startActivity(new Intent(MainActivity.this, EditConfigActivity.class));
+                return true;
+            case R.id.action_delete_config:
+                startActivity(new Intent(MainActivity.this, DeleteConfig.class));
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
